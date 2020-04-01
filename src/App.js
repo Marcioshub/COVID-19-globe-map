93c4c0b803e25cfc4f23cc819c9d7ad805afc939
Marcio <mecastillo00@gmail.com>
2020-04-01 13:27:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from "react";
+import "./App.css";
+import Globe from "react-globe.gl";
+import * as d3 from "d3";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState({ features: [] });
+  const [hoverD, setHoverD] = useState();
+
+  const [data, setData] = useState("");
+
+  async function getData() {
+    const temp = await axios.get("https://api.covid19api.com/summary");
+    setData(temp);
+  }
+
+  function findCountryData(name) {
+    switch (name) {
+      case "United States of America":
+        name = "US";
+        break;
+
+      case "Dominican Rep.":
+        name = "Dominican Republic";
+        break;
+
+      case "Bosnia and Herz.":
+        name = "Bosnia and Herzegovina";
+        break;
+
+      case "Dem. Rep. Congo":
+        name = "Congo (Kinshasa)";
+        break;
+
+      case "Central African Rep.":
+        name = "Central African Republic";
+        break;
+
+      default:
+        break;
+    }
+
+    for (let i = 0; i < data.data.Countries.length; i++) {
+      if (data.data.Countries[i].Country.includes(name)) {
+        return `
+        Confirmed Cases: ${data.data.Countries[i].TotalConfirmed} <br />
+        Total Deaths: ${data.data.Countries[i].TotalDeaths} <br />
+        Total Recovered: ${data.data.Countries[i].TotalRecovered}
+        `;
+      }
+    }
+
+    return "NO DATA FOUND...";
+  }
+
+  useEffect(() => {
+    // load data
+    axios
+      .get("../datasets/ne_110m_admin_0_countries.geojson")
+      .then(function(response) {
+        // handle success
+        //console.log(response.data);
+        getData();
+        setCountries(response.data);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
+
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+
+  // GDP per capita (avoiding countries with small pop)
+  const getVal = feat =>
+    feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+
+  const maxVal = useMemo(() => Math.max(...countries.features.map(getVal)), [
+    countries
+  ]);
+  colorScale.domain([0, maxVal]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Globe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      polygonsData={countries.features}
+      polygonAltitude={d => (d === hoverD ? 0.12 : 0.06)}
+      polygonCapColor={d =>
+        d === hoverD ? "steelblue" : colorScale(getVal(d))
+      }
+      polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+      polygonStrokeColor={() => "#111"}
+      polygonLabel={({ properties: d }) => `
+    <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+    GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+    Population: <i>${d.POP_EST}</i>
+    <br />
+    ${findCountryData(d.ADMIN)}
+  `}
+      onPolygonHover={setHoverD}
+      polygonsTransitionDuration={300}
+    />
   );
 }
 
